fc901d3a5da13c25d47e88ba739fad5aa711040f
JustineCarida <422004903@ntc.edu.ph>
2026-09-09 23:48:17 +0800

    Initial C++ student information system

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Display the student information system header and details
+    cout << "==============================" << endl;
+    cout << "    STUDENT INFORMATION SYSTEM" << endl;
+    cout << "==============================" << endl;
+    cout << endl;
+
+    cout << "Name: Justine Carida" << endl;
+    cout << "Course: BSIT" << endl;
+    cout << "Year Level: 3" << endl;
+    cout << endl;
+
+    cout << "Welcome to the system!" << endl;
+
+    return 0;
+}
